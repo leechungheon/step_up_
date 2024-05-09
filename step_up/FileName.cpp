c080fcc2e163c452ea159e4bceb5f5c38aed8202
dawn_lch <chung4111@naver.com>
2024-05-09 20:46:04 +0900

    first

--- step_up/FileName.cpp
+++ step_up/FileName.cpp
@@ -1,11 +1,17 @@
 #include <stdio.h>
+int factorial(int n);
 int main() {
-    char text;
-
-    scanf_s("%c", &text);
-    printf("%d\n", text);
-    printf("%c\n", text);
-    printf("%.2f\n", text);
-
+    int n;
+    scanf_s("%d", &n);
+    factorial(n);
+    printf("%d", factorial(n));
     return 0;
+}
+int factorial(int n) {
+    if (n == 1) {
+        return 1;
+    }
+    else {
+        return n * factorial(n - 1);
+    }
 }
