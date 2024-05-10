182ef1cb5097c46f743b64ce48064aef8f1d2473
dawn_lch <chung4111@naver.com>
2024-05-10 16:44:54 +0900

    stack

--- step_up/FileName.cpp
+++ step_up/FileName.cpp
@@ -1,22 +1,70 @@
-#include <stdio.h>
-int sum(int array[][5]) {
-    int total = 0;
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < 5; j++) {
-            total += array[i][j];
-        }
-    }
-    return total;
-}
-int main() {
-    int array[5][5];
-    int num;
-    scanf_s("%d", &num);
-    for (int i = 0; i <= num; i++) {
-        for (int j = 0; j <= num; j++) {
-            array[i][j] = num;
-        }
-    }
-    printf("%d", sum(array));
+#include<stdio.h>
+
+#define SIZE 100
+
+int stack[SIZE], choice, top;
+
+void push(int x);
+
+int pop(void);
+
+void display(void);
+
+int main()
+
+{
+
+    top = -1;
+
+
+
+    push(1);
+
+    push(2);
+
+    push(3);
+
+    push(4);
+
+    push(5);
+
+    display();
+
+
+
+    pop();
+
+    pop();
+
+    display();
+
+
+
     return 0;
+
+}
+
+void push(int x)
+
+{
+    stack[++top]=x;
+    choice = top;
+}
+
+int pop()
+
+{
+    int num;
+    num = stack[top];
+    stack[top--] = 0;
+    choice = top;
+    return num;
+}
+
+void display()
+
+{
+    for (int i = choice; i >= 0; i--) {
+        printf("%d\n", stack[i]);
+    }
 }
