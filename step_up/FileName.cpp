7d6bf3938170be713758f236824a562691da4202
dawn_lch <chung4111@naver.com>
2024-05-10 17:59:12 +0900

    first

--- step_up/FileName.cpp
+++ step_up/FileName.cpp
@@ -1,22 +1,34 @@
-#include<stdio.h>
+#include <stdio.h>
 
-#define SIZE 100
-
-int stack[SIZE], choice, top;
-
-void push(int x);
-
-int pop(void);
-
-void display(void);
-
-int main()
-
-{
-
-    top = -1;
+#include <stdlib.h>
 
 
+
+void push(int);
+
+int pop();
+
+void print_s();
+
+
+
+typedef struct node {
+
+    int data;
+
+    struct node* next;
+
+}stack;
+
+
+
+
+
+stack* top = NULL;
+
+
+
+int main() {
 
     push(1);
 
@@ -28,7 +40,7 @@ int main()
 
     push(5);
 
-    display();
+    print_s();
 
 
 
@@ -36,35 +48,54 @@ int main()
 
     pop();
 
-    display();
-
-
-
-    return 0;
+    print_s();
 
 }
 
-void push(int x)
 
-{
-    stack[++top]=x;
-    choice = top;
-}
 
-int pop()
-
-{
-    int num;
-    num = stack[top];
-    stack[top--] = 0;
-    choice = top;
-    return num;
-}
-
-void display()
-
-{
-    for (int i = choice; i >= 0; i--) {
-        printf("%d\n", stack[i]);
+void push(int data) {
+    stack *newNode = (stack*)malloc(sizeof(stack));
+    //struct node* newNode; 기존 오류 코드 수정
+    if (newNode == NULL) {
+        printf("Overstack!\n");
+        exit(1);
     }
+    newNode->data = data;
+    newNode->next = top;
+    top = newNode;
 }
+
+                                                                                                                           
+int pop() {
+    stack* del;
+    int value;
+
+    if (top == NULL) {
+        printf("stack is empty!");
+        exit(1);
+    }
+    value = top->data;
+    del = top;
+    top = top->next;
+    free(del);
+
+    return value;
+}
+                                                                                                                             
+void print_s() {
+    stack* i;
+    stack* count = top;
+
+    i = count;
+    while (i != NULL) {
+
+        printf("%d \n", i->data);
+        i = count->next;
+        count = count->next;
+
+    }
+
+    printf("\n");
+}
+
