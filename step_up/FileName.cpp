d173134ee9901a5a9560491a8d7334e0b97857e7
dawn_lch <chung4111@naver.com>
2024-05-14 13:19:01 +0900

    binarytree

--- step_up/FileName.cpp
+++ step_up/FileName.cpp
@@ -1,37 +1,196 @@
+#include<stdio.h> 
+#include<stdlib.h> 
 
-#include<stdio.h>
-#define SIZE 100
-int stack[SIZE], choice, top;
-void push(int x) {
-    stack[++top] = x;
-    choice = top;
+typedef char element;		// 이진 탐색 트리 element의 자료형을 char로 정의
+typedef struct treeNode {
+	char key;				// 데이터 필드
+	struct treeNode* left;	// 왼쪽 서브 트리 링크 필드
+	struct treeNode* right;	// 오른쪽 서브 트리 링크 필드
+} treeNode;
+
+typedef struct QNode {
+	treeNode* data;
+	struct QNode* link;
+} QNode;
+
+// 큐 구조체 정의
+typedef struct {
+	QNode* front, * rear;
+} LQueueType;
+
+LQueueType* createLinkedQueue() {
+	LQueueType* LQ;
+	LQ = (LQueueType*)malloc(sizeof(LQueueType));
+	LQ->front = NULL;
+	LQ->rear = NULL;
+	return LQ;
 }
-int pop(void) {
-    int num = stack[top];
-    stack[top--] = 0;
-    choice--;
-    return num;
+
+int isEmpty(LQueueType* LQ) {
+	if (LQ->front == NULL) {
+		//printf("Linked Queue is Empty!");
+		return 1;
+	}
+	else return 0;
 }
-void display(void) {
-    int i;
-    if (top >= 0) {
-        for (i = top; i >= 0; i--)
-            printf("%d ", stack[i]);
-    }
-    else {
-        printf("\n STACK Empty");
-    }
+
+
+void enqueue(LQueueType* LQ, treeNode* data) {
+	QNode* newNode = (QNode*)malloc(sizeof(QNode));
+	newNode->data = data;
+	newNode->link = NULL;
+
+	if (isEmpty(LQ)) {
+		LQ->front = newNode;
+		LQ->rear = newNode;
+	}
+	else {
+		LQ->rear->link = newNode;
+		LQ->rear = newNode;
+	}
+}
+
+treeNode* dequeue(LQueueType* LQ) {
+	QNode* old = LQ->front;
+	treeNode* item;
+	if (isEmpty(LQ)) return 0;
+	else {
+		item = old->data;
+		LQ->front = LQ->front->link;
+	}
+	if (LQ->front == NULL) LQ->rear = NULL;
+	free(old);
+	return item;
+}
+// 이진 탐색 트리에서 키값이 x인 노드의 위치를 탐색하는 연산
+treeNode* searchBST(treeNode* root, char x) {
+	treeNode* p;
+	p = root;
+	while (p != NULL) {
+		if (x < p->key) p = p->left;
+		else if (x == p->key) return p;
+		else p = p->right;
+	}
+	printf("\n 찾는 키가 없습니다!");
+	return p;
+}
+
+// 포인터 p가 가리키는 노드와 비교하여 노드 x를 삽입하는 연산
+treeNode* insertNode(treeNode* p, char x) {
+	treeNode* newNode;
+	if (p == NULL) {
+		newNode = (treeNode*)malloc(sizeof(treeNode));
+		newNode->key = x;
+		newNode->left = NULL;
+		newNode->right = NULL;
+		return newNode;
+	}
+	else if (x < p->key)  p->left = insertNode(p->left, x);
+	else if (x > p->key)  p->right = insertNode(p->right, x);
+	else  printf("\n 이미 같은 키가 있습니다! \n");
+
+	return p;
+}
+
+// 루트 노드부터 탐색하여 키값과 같은 노드를 찾아 삭제하는 연산
+void deleteNode(treeNode* root, element key) {
+	treeNode* parent, * p, * succ, * succ_parent;
+	treeNode* child;
+
+	parent = NULL;
+	p = root;
+	while ((p != NULL) && (p->key != key)) {  // 삭제할 노드의 위치 탐색
+		parent = p;
+		if (key < p->key) p = p->left;
+		else p = p->right;
+	}
+
+	// 삭제할 노드가 없는 경우
+	if (p == NULL) {
+		printf("\n 찾는 키가 이진 트리에 없습니다!!");
+		return;
+	}
+
+	// 삭제할 노드가 단말 노드인 경우
+	if ((p->left == NULL) && (p->right == NULL)) {
+		if (parent != NULL) {
+			if (parent->left == p) parent->left = NULL;
+			else parent->right = NULL;
+		}
+		else root = NULL;
+	}
+
+	// 삭제할 노드가 자식 노드를 한 개 가진 경우
+	else if ((p->left == NULL) || (p->right == NULL)) {
+		if (p->left != NULL) child = p->left;
+		else child = p->right;
+
+		if (parent != NULL) {
+			if (parent->left == p) parent->left = child;
+			else parent->right = child;
+		}
+		else root = child;
+	}
+
+	// 삭제할 노드가 자식 노드를 두 개 가진 경우
+	else {
+		succ_parent = p;
+		succ = p->left;
+		while (succ->right != NULL) { // 왼쪽 서브 트리에서 후계자 찾기
+			succ_parent = succ;
+			succ = succ->right;
+		}
+		if (succ_parent->left == succ)  succ_parent->left = succ->left;
+		else succ_parent->right = succ->left;
+		p->key = succ->key;
+		p = succ;
+	}
+	free(p);
+}
+
+// 이진 탐색 트리를 중위 순회하면서 출력하는 연산
+void displayInorder(treeNode* root) {
+	if (root) {
+		displayInorder(root->left);
+		printf("%c_", root->key);
+		displayInorder(root->right);
+	}
+}
+void displayLevelorder(treeNode* root) {
+	LQueueType queue;
+	createLinkedQueue();
+	enqueue(&queue, root);
+
+	while (!isEmpty(&queue)) {
+		treeNode* node = dequeue(&queue);
+		printf("%c_", node->key);
+
+		if (node->left)
+			enqueue(&queue, node->left);
+		if (node->right)
+			enqueue(&queue, node->right);
+	}
 }
 int main() {
-    int i;
-    top = -1;
-    int num;
+	treeNode* root = NULL;
+	treeNode* foundedNode = NULL;
 
-    for (i = 1; i <= 3; i++) {
-        scanf_s("%d", &num);
-        push(num);
-    }
-    pop();
-    display();
-    return 0;
+	// [그림 7-38]과 같은 초기 이진 탐색 트리를 구성하고 
+	// 노드 G를 루트 노드 포인터 root로 지정
+	root = insertNode(root, 'G');
+	insertNode(root, 'I');
+	insertNode(root, 'H');
+	insertNode(root, 'D');
+	insertNode(root, 'B');
+	insertNode(root, 'M');
+	insertNode(root, 'N');
+	insertNode(root, 'A');
+	insertNode(root, 'J');
+	insertNode(root, 'E');
+	insertNode(root, 'Q');
+
+	displayInorder(root);
+	printf("\n");
+
+	displayLevelorder(root);
 }
