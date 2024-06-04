2c0536a74634965bc815ff4d7cd9f2ef255dbdd6
dawn_lch <chung4111@naver.com>
2024-06-04 20:02:01 +0900

    first

--- step_up/FileName.cpp
+++ step_up/FileName.cpp
@@ -1,62 +1,69 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define Q_SIZE 4
 
-typedef char element;
-typedef struct {
-    element queue[Q_SIZE];
-    int front, rear;
-}QueueType;
+#define TRUE 1
+#define FALSE 0
+#define N 10
 
-QueueType* creatQueue() {
-    QueueType* Q;
-    Q = (QueueType*)malloc(sizeof(QueueType));
-    Q->front = -1;
-    Q->rear = -1;
-    return Q;
+struct list_node {
+	int vertex;
+	int weight;
+	struct list_node* link;
+};
+
+typedef struct list_node node;
+typedef node* node_ptr;
+node_ptr graph[N];
+int visited[N];
+/* Matrix 인접행렬, 경로가 없으면 0, 경로가 있으면 경로 길이 값 */
+int matrix[N][N] = { { 0,1,1,0,1,0,1,0,0,0 },       { 1,0,0,0,0,0,0,0,0,0 },        { 1,0,0,1,0,0,0,0,1,0 },       { 0,0,1,0,0,1,0,1,0,0 },
+{ 1,0,0,0,0,0,0,0,0,0 },      { 0,0,0,1,0,0,0,0,0,1 },      { 1,0,0,0,0,0,0,1,0,0 },     { 0,0,0,1,0,0,1,0,0,0 },     { 0,0,1,0,0,0,0,0,0,0 },
+{ 0,0,0,0,0,1,0,0,0,0 },
+};
+/* create 함수는 인접행렬을 인접리스트로 변환한다 */
+void create()
+{
+	int i, j;
+	node_ptr temp;
+	for (i = 0; i < 10; i++)
+		for (j = N - 1; j >= 0; j--)
+		{
+			if (matrix[i][j] != 0)
+			{
+				temp = (node_ptr)malloc(sizeof(node));
+				temp->link = graph[i];
+				temp->weight = matrix[i][j];
+				temp->vertex = j;
+				graph[i] = temp;
+			}
+		}
 }
-
-int isEmpty(QueueType* Q) {
-    if (Q->front == Q->rear) {
-        printf("Queue is empty!");
-        return 1;
-    }
-    else return 0;
+void dfs(int v)
+{
+	node_ptr  w;
+	visited[v] = TRUE; /* 1 방문지 표시 */
+	printf("%5d", v);
+	for (w = graph[v]; w; w = w->link) /* 2 연결리스트 탐색 */
+		if (!visited[w->vertex])
+			dfs(w->vertex);
 }
-
-int isFull(QueueType* Q) {
-    if (Q->rear == Q_SIZE - 1) {
-        printf("Queue is full!");
-        return 1;
-    }
-    else return 0;
-}
-
-void enQueue(QueueType* Q, element item) {
-    if (isFull(Q))return;
-    else {
-        Q->rear++;
-        Q->queue[Q->rear] = item;
-    }
-}
-
-element deQueue(QueueType* Q, element item) {
-    if (isEmpty(Q))return;
-    else {
-        Q->front++;
-        return Q->queue[Q->front];
-    }
-}
-
-element peek(QueueType* Q) {
-    if (isEmpty)exit(1);
-    else return  Q->queue[Q->front + 1];
-}
-
-void printQ(QueueType* Q) {
-    int i;
-    printf("Queue :[");
-    for (i = Q->front + 1; i <= Q->rear; i++)
-        printf("%3c", Q->queue[i]);
-    printf("]");
+void main()
+{
+	int i;
+	node_ptr temp;
+	/* 초기화 작업 */
+	for (i = 0; i < 10; i++) { graph[i] = NULL; visited[i] = FALSE; }
+	create();
+	printf(" * 연결리스트 내용 출력 * \n");
+	for (i = 0; i < 10; i++)
+	{
+		printf("* %dth node = ", i);
+		temp = graph[i];
+		while (temp) { printf(" %d", temp->vertex);  temp = temp->link; };
+		printf("\n");
+	}
+	/* dfs 탐색, 3번 노드부터 시작해본다 */
+	printf(" \n * DFS Traversal is = ");
+	dfs(3);
+	printf(" end of  DFS Traversal\n");
 }
